c14a98dbe68a7c5d83974cd1e0869cdcc8fd0ed8
Tuvpt <32826247+Tuvpt@users.noreply.github.com>
2019-08-20 14:56:17 +0700

    fix css

--- view/adminhtml/web/js/grid/columns/select.js
+++ view/adminhtml/web/js/grid/columns/select.js
@@ -35,16 +35,20 @@ define([
 
             switch (record[this.index]){
                 case 'pending':
-                    result = '<span class="mp-grid-severity-pending"><span>' + $t('PENDING') + '</span></span>';
+                    result = '<span class="mp-grid-severity mp-grid-severity-pending">' +
+                        '<span>' + $t('PENDING') + '</span></span>';
                     break;
                 case 'error':
-                    result = '<span class="mp-grid-severity-error"><span>' + $t('ERROR') + '</span></span>';
+                    result = '<span class="mp-grid-severity mp-grid-severity-error">' +
+                        '<span>' + $t('ERROR') + '</span></span>';
                     break;
                 case 'success':
-                    result = '<span class="mp-grid-severity-success"><span>' + $t('SUCCESS') + '</span></span>';
+                    result = '<span class="mp-grid-severity mp-grid-severity-success">' +
+                        '<span>' + $t('SUCCESS') + '</span></span>';
                     break;
                 case 'skipped':
-                    result = '<span class="mp-grid-severity-skip"><span>' + $t('SKIPPED') + '</span></span>';
+                    result = '<span class="mp-grid-severity mp-grid-severity-skip">' +
+                        '<span>' + $t('SKIPPED') + '</span></span>';
                     break;
             }
 
